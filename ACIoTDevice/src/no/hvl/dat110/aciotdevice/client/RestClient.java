b7065044c85fabe05f16df2b8c25756ae488f100
PaTrond <trond@hauge.app>
2021-04-30 02:44:48 +0200

    WIP - Mulig call-feil under samkjøring i Eclipse og VSCode

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,27 +1,57 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class RestClient {
 
 	public RestClient() {
-		// TODO Auto-generated constructor stub
 	}
 
-	private static String logpath = "/accessdevice/log";
+	private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+	private static String logpath = "/accessdevice/log/";
+
+	private String host = Configuration.host;
+	private int port = Configuration.port;
+
+	private String url = "http://" + host + ":" + port;
+	private String urlPath = url + logpath;
 
 	public void doPostAccessEntry(String message) {
 
-		// TODO: implement a HTTP POST on the service to post the message
+
+		System.out.println(urlPath);
 		
+		String jsonString = new Gson().toJson(new AccessMessage(message));
+
+		RequestBody reqBody = RequestBody.create(JSON, jsonString);
+
+		OkHttpClient client = new OkHttpClient();
+
+		Request req = new Request.Builder().url(urlPath).post(reqBody).build();
+
+		try (Response res = client.newCall(req).execute()) {
+			System.out.println(res.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	private static String codepath = "/accessdevice/code";
-	
+
 	public AccessCode doGetAccessCode() {
 
 		AccessCode code = null;
-		
+
 		// TODO: implement a HTTP GET on the service to get current access code
-		
+
 		return code;
 	}
 }
